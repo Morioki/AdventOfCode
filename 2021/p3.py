9b9c84f1a5d5ce3462f71ac96d0e6c5f15b20824
Garrett Braatz <morioki@protonmail.com>
2021-12-09 18:26:03 -0800

    aoc day 3 complete

--- 2021/p3.py
+++ 2021/p3.py
@@ -7,33 +7,79 @@ def parse(puzzle_input):
     return [str(line) for line in puzzle_input.split()]
     # return [tuple(line.split()) for line in puzzle_input.split('\n')]
 
-def calcGammaAndEpsilon(data):
+def getZeroOneFreq(data, pos):
+    zeroFreq = 0
+    oneFreq = 0
+    for el in data:
+        oneFreq += 1 if el[pos] == "1" else 0
+        zeroFreq += 1 if el[pos] == "0" else 0
+
+    return zeroFreq, oneFreq
+
+def parseGammaAndEpsilon(data):
     gammaRate = ""
     epsilonRate = ""
 
     for pos in range(len(data[1])):
-        zeroFreq = 0
-        oneFreq = 0
-        for el in data:
-            oneFreq += 1 if el[pos] == "1" else 0
-            zeroFreq += 1 if el[pos] == "0" else 0
+        zeroFreq, oneFreq = getZeroOneFreq(data, pos)
         
-        print(f'Position {pos}:\n One Freq: {oneFreq}\n Zero Freq: {zeroFreq}')
+        # print(f'Position {pos}:\n One Freq: {oneFreq}\n Zero Freq: {zeroFreq}')
         gammaRate += "1" if oneFreq >= zeroFreq else "0"
         epsilonRate += "1" if oneFreq <= zeroFreq else "0"
 
-    pass
-    
+    return gammaRate, epsilonRate
+
+def parseOxyRating(data):
+    workingSet = data
+    for pos in range(len(workingSet[1])):
+        # print(f'Checking pos: {pos}')
+        if len(workingSet) == 1:
+            return workingSet[0]
+
+        zeroFreq, oneFreq = getZeroOneFreq(workingSet,pos)
+        # print(f' Zero Freq: {zeroFreq}\n One Freq: {oneFreq}')
+        if zeroFreq > oneFreq:
+            # Keep the records with 0 in pos
+            workingSet = [ record for record in workingSet if record[pos] == "0"]
+        else:
+            workingSet = [ record for record in workingSet if record[pos] == "1"]
+
+    return workingSet[0]
+
+def parseCO2Rating(data):
+    workingSet = data
+    for pos in range(len(workingSet[1])):
+        # print(f'Checking pos: {pos}')
+        if len(workingSet) == 1:
+            return workingSet[0]
+
+        zeroFreq, oneFreq = getZeroOneFreq(workingSet,pos)
+        # print(f' Zero Freq: {zeroFreq}\n One Freq: {oneFreq}')
+        if zeroFreq <= oneFreq:
+            # Keep the records with 0 in pos
+            workingSet = [ record for record in workingSet if record[pos] == "0"]
+        else:
+            workingSet = [ record for record in workingSet if record[pos] == "1"]
+
+    return workingSet[0] 
 
 def part1(data):
     """Solve part 1"""
-    gamma = calcGammaAndEpsilon(data)
-    # print(data)
-    pass
+    gamma, epsilon = parseGammaAndEpsilon(data)
+    gammaInt = int(gamma, 2)
+    epsilonInt = int(epsilon, 2)
+
+    return gammaInt * epsilonInt
 
 def part2(data):
     """Solve part 2"""
-    pass
+    oxyRating = parseOxyRating(data)
+    co2Rating = parseCO2Rating(data)
+
+    oxyInt = int(oxyRating, 2)
+    co2Int = int(co2Rating, 2)
+
+    return oxyInt * co2Int
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input"""
